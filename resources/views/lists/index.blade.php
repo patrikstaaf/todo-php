8044302fecfbb671b9e0f39720dae7cefb9ca3a6
patrikstaaf <staaf.patrik@gmail.com>
2021-12-20 14:44:23 +0100

    Added ide helper + minor random styling

--- resources/views/lists/index.blade.php
+++ resources/views/lists/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="w-8/12 flex justify-between mx-auto">
+    <button class="bg-green-300 hover:bg-green-400 text-gray-800 font-bold py-2 px-4 rounded mb-6">Create new list</button>
+    <button class="bg-red-300 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded mb-6">Delete all lists</button>
+</div>
+
 <div class="flex justify-center">
     <div class="w-8/12 bg-white p-6 rounded-lg">
         Lists
